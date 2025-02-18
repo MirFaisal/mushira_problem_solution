461775cefad093bd9da0b8d76b8c811500aea6fc
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-02-19 01:34:30 +0600

    add isBestFriend function to validate friendship criteria

--- index.js
+++ index.js
@@ -60,3 +60,17 @@ function electionResult(votes) {
 }
 
 // problem 4
+function isBestFriend(f1, f2) {
+  if (typeof f1 === "object" && typeof f2 === "object") {
+    if (Object.keys(f1).length !== 3 || Object.keys(f2).length !== 3) {
+      return "Invalid";
+    }
+
+    if (f1.bestFriend === f2.roll || f2.bestFriend === f1.roll) {
+      return true;
+    }
+  } else {
+    return "Invalid";
+  }
+}
+
