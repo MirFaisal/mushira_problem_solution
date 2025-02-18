e66bf7e0b1d5653d551984ec90a43d94c01cdb08
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-02-19 00:56:22 +0600

    add validEmail function to validate email addresses

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+// problem 1
 function cashOut(amount) {
   const cashOutChargeRate = 1.75;
   if (typeof amount === "number" && amount >= 0) {
     return (amount * cashOutChargeRate) / 100;
+  } else {
+    return "Invalid";
+  }
+}
+
+// problem 2
+function validEmail(email) {
+  if (typeof email === "string") {
+    if (
+      email.charAt(0).includes(".") ||
+      email.charAt(0).includes("-") ||
+      email.charAt(0).includes("_") ||
+      email.charAt(0).includes("@") ||
+      email.charAt(0).includes("+")
+    ) {
+      return false;
+    }
+
+    if (!email.includes("@")) {
+      return false;
+    }
+    if (!email.endsWith(".com")) {
+      return false;
+    }
+    return true;
   } else {
     return "Invalid";
   }
